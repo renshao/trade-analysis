bb124334b5fa34824d7cfb2b2b581f356434cdef
Ren Shao <rshao@seek.com.au>
2021-06-30 21:39:01 +1000

    Print trade transactions in table

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+#[macro_use] extern crate prettytable;
 use std::error::Error;
-use std::io;
+use std::{io, fmt};
 use std::process;
 use std::fs::File;
-
 use serde::Deserialize;
+use prettytable::{Table, Row, Cell};
+use prettytable::format;
+use prettytable::format::Alignment;
+
 
 #[derive(Debug, Deserialize)]
 enum TradeType {
     BUY,
     SELL,
+}
+impl fmt::Display for TradeType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            TradeType::BUY => write!(f, "BUY"),
+            TradeType::SELL => write!(f, "SELL"),
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -22,15 +34,32 @@ struct TradingRecord {
     fee: f32
 }
 
+fn calculateTotal(price: f32, volume: i32, fee: f32) -> f32 {
+    price * (volume as f32) + fee
+}
+
 fn read_transactions() -> Result<(), Box<dyn Error>> {
-    // Build the CSV reader and iterate over each record.
+    let mut table = Table::new();
+    table.set_titles(row!["Date", "Trade", "Code", "Volume", "Price", "Fee", "Total"]);
+
     let mut rdr = csv::Reader::from_reader(io::BufReader::new(File::open("trades.csv")?));
     for result in rdr.deserialize() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here..
         let record: TradingRecord = result?;
-        println!("{:?}", record);
+        let mut row = row![record.date, record.buy_or_sell, record.code];
+        row.add_cell(Cell::new_align(&record.volume.to_string(), Alignment::RIGHT));
+        row.add_cell(Cell::new_align(&format!("{:.3}", &record.price), Alignment::RIGHT));
+        row.add_cell(Cell::new_align(&format!("{:.2}", record.fee), Alignment::RIGHT));
+        let mut totalString = format!("{:.2}", calculateTotal(record.price, record.volume, record.fee));
+        match record.buy_or_sell {
+            TradeType::BUY => totalString.insert_str(0, "- "),
+            _ => (),
+        }
+        row.add_cell(Cell::new_align(&totalString, Alignment::RIGHT));
+        table.add_row(row);
     }
+
+    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.printstd();
     Ok(())
 }
 
