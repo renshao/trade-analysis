f3dfcf4b7c45cefed247c34f104d60e5f48ec441
Ren Shao <rshao@seek.com.au>
2021-07-07 22:59:45 +1000

    Refactor + print acquired duration

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,13 +1,7 @@
 use std::collections::HashMap;
-use chrono::NaiveDateTime;
+use chrono::{NaiveDateTime, Datelike, Duration};
 use crate::{TradingRecord, TradeType};
-
-struct BuyItem {
-    date: NaiveDateTime,
-    shares: u32,
-    price: f32,
-    remaining_fee: f32
-}
+use crate::TradeType::{BUY, DIV, SELL};
 
 struct InventoryItem {
     date_acquired: NaiveDateTime,
@@ -16,71 +10,66 @@ struct InventoryItem {
     remaining_fee: f32
 }
 
-pub struct Fulfillment {
-    // quantity, bought price, buying transaction fee
-    pub(crate) items: Vec<(u32, f32, f32)>,
-    pub(crate) net_profit: f32
-}
-
 // Output of Inventory is a list of ConsolidatedTransaction in chronological ordered
 pub struct ConsolidatedTransaction {
-    date: NaiveDateTime,
-    trade_type: TradeType,
-    code: String,
-    quantity: u32,
-    price: f32,
-    fee: f32,
-    amount_settled: f32, // for buy: quantity * price + fee, for sell: quantity * price - fee
-    fulfillments: Option<Vec<SellingFulfillment>>,
-    net_profit: f32
+    pub date: NaiveDateTime,
+    pub trade_type: TradeType,
+    pub code: String,
+    pub quantity: u32,
+    pub price: f32,
+    pub fee: f32,
+    pub amount_settled: f32, // for buy: quantity * price + fee, for sell: quantity * price - fee
+    pub fulfillments: Option<Vec<SellingFulfillment>>,
+    pub net_profit: f32
 }
 
 pub struct SellingFulfillment {
-    date_purchased: NaiveDateTime,
-    purchase_price: f32,
-    quantity: u32,
-    purchase_fee: f32,
-    selling_fee: f32
+    pub date_purchased: NaiveDateTime,
+    pub purchase_price: f32,
+    pub quantity: u32,
+    pub purchase_fee: f32,
+    pub selling_fee: f32,
+    pub acquired_duration: Duration,
+    pub profit: f32
 }
 
 pub struct Inventory {
     inventory_items: HashMap<String, Vec<InventoryItem>>,
-    shares_map: HashMap<String, Vec<BuyItem>>,
     // financial year -> profit
-    pub(crate) fy_profit_map: HashMap<u32, f32>
+    pub(crate) fy_profit_map: HashMap<u32, f32>,
+    consolidated_transactions: Vec<ConsolidatedTransaction>
 }
 
 impl Inventory {
     pub fn new() -> Inventory {
         Inventory {
             inventory_items: HashMap::new(),
-            shares_map: HashMap::new(),
-            fy_profit_map: HashMap::new()
+            fy_profit_map: HashMap::new(),
+            consolidated_transactions: vec![]
         }
     }
 
-    // pub fn consolidated_transactions(&self) -> Vec<ConsolidatedTransaction> {
-    //
-    // }
+    pub fn consolidated_transactions(&self) -> &Vec<ConsolidatedTransaction> {
+        &self.consolidated_transactions
+    }
 
     pub fn record_transaction(&mut self, trading_record: &TradingRecord) {
-        let code = String::from(&trading_record.code);
-        let inventory_items = self.inventory_items.entry(code).or_insert(Vec::new());
-
         match trading_record.buy_or_sell {
             TradeType::BUY => {
-                Inventory::record_buy(inventory_items, trading_record);
+                self.record_buy(trading_record);
             },
             TradeType::SELL => {
-
+                self.record_sell(trading_record);
             },
             TradeType::DIV => {
-
+                self.record_dividend(trading_record);
             }
         }
     }
 
-    fn record_buy(inventory_items: &mut Vec<InventoryItem>, trading_record: &TradingRecord) {
+    fn record_buy(&mut self, trading_record: &TradingRecord) {
+        let inventory_items = self.inventory_items.entry(String::from(&trading_record.code)).or_insert(Vec::new());
+
         let mut i: usize = 0;
         for item in inventory_items.iter() {
             if trading_record.date < item.date_acquired {
@@ -94,61 +83,99 @@ impl Inventory {
             price: trading_record.price,
             remaining_fee: trading_record.fee
         });
-    }
 
-    pub fn buy(&mut self, date: NaiveDateTime, code: &str, volume: u32, price: f32, fee: f32) {
-        if !self.shares_map.contains_key(code) {
-            self.shares_map.insert(String::from(code), Vec::new());
-        }
-
-        self.shares_map.get_mut(code).unwrap().push(BuyItem {
-            date,
-            shares: volume,
-            price: price,
-            remaining_fee: fee
+        self.consolidated_transactions.push(ConsolidatedTransaction {
+            date: trading_record.date,
+            trade_type: BUY,
+            code: String::from(&trading_record.code),
+            quantity: trading_record.volume,
+            price: trading_record.price,
+            fee: trading_record.fee,
+            amount_settled: trading_record.price * trading_record.volume as f32 + trading_record.fee,
+            fulfillments: None,
+            net_profit: 0.0
         });
     }
 
-    pub fn sell(&mut self, fy: u32, code: &str, volume: u32, price: f32, fee: f32) -> Fulfillment {
-        let mut items = Vec::new();
-        let stocks = self.shares_map.get_mut(code).unwrap();
-        let mut quantity_to_fulfill = volume;
-        let mut net_profit = -fee;
-        loop {
-            let first_stock = stocks.get_mut(0).unwrap();
-            let q = if quantity_to_fulfill <= first_stock.shares {quantity_to_fulfill} else {first_stock.shares};
-            net_profit += q as f32 * (price - first_stock.price);
-            quantity_to_fulfill -= q;
-            first_stock.shares -= q;
+    fn record_sell(&mut self, trading_record: &TradingRecord) {
+        let inventory_items = self.inventory_items.get_mut(&trading_record.code).unwrap();
+        let mut quantity_to_fulfill = trading_record.volume;
+        let mut net_profit = 0.0;
+        let mut fulfillments: Vec<SellingFulfillment> = vec![];
 
-            let buying_transaction_fee;
-            if first_stock.remaining_fee > 0.0 {
-                buying_transaction_fee = first_stock.remaining_fee;
-                first_stock.remaining_fee = 0.0;
-                net_profit -= buying_transaction_fee;
+        let mut i = 0;
+        loop {
+            let earliest_item = inventory_items.get_mut(0).unwrap();
+            let current_round_quantity = if quantity_to_fulfill <= earliest_item.quantity { quantity_to_fulfill } else { earliest_item.quantity };
+            quantity_to_fulfill -= current_round_quantity;
+            earliest_item.quantity -= current_round_quantity;
+
+            let purchase_fee;
+            if earliest_item.remaining_fee > 0.0 {
+                purchase_fee = earliest_item.remaining_fee;
+                earliest_item.remaining_fee = 0.0;
             } else {
-                buying_transaction_fee = 0.0;
+                purchase_fee = 0.0;
             }
-            items.push((q, first_stock.price, buying_transaction_fee));
-            if first_stock.shares == 0 {
-                stocks.remove(0);
+
+            let selling_fee = if i == 0 { trading_record.fee } else { 0.0 };
+
+            let fulfillment_profit = (trading_record.price - earliest_item.price) * current_round_quantity as f32 - purchase_fee - selling_fee;
+            fulfillments.push(SellingFulfillment {
+                date_purchased: earliest_item.date_acquired,
+                purchase_price: earliest_item.price,
+                quantity: current_round_quantity,
+                purchase_fee,
+                selling_fee,
+                acquired_duration: trading_record.date - earliest_item.date_acquired,
+                profit: fulfillment_profit
+            });
+
+            net_profit += fulfillment_profit;
+
+            if earliest_item.quantity == 0 {
+                inventory_items.remove(0);
             }
 
             if quantity_to_fulfill == 0 {
                 break;
             }
+
+            i += 1;
         }
 
-        self.record_fy_profit(fy, net_profit);
-
-        Fulfillment {
-            items,
+        self.consolidated_transactions.push(ConsolidatedTransaction {
+            date: trading_record.date,
+            trade_type: SELL,
+            code: String::from(&trading_record.code),
+            quantity: trading_record.volume,
+            price: trading_record.price,
+            fee: trading_record.fee,
+            amount_settled: trading_record.price * trading_record.volume as f32 - trading_record.fee,
+            fulfillments: Some(fulfillments),
             net_profit
-        }
+        });
+
+        let financial_year = if trading_record.date.month() < 7 {trading_record.date.year()} else {trading_record.date.year() + 1} as u32;
+        self.record_fy_profit(financial_year, net_profit);
     }
 
-    pub fn record_dividend(&mut self, fy: u32, _code: &str, volume: u32, amount: f32) {
-        self.record_fy_profit(fy, volume as f32 * amount);
+    pub fn record_dividend(&mut self, trading_record: &TradingRecord) {
+        let financial_year = if trading_record.date.month() < 7 {trading_record.date.year()} else {trading_record.date.year() + 1} as u32;
+        self.record_fy_profit(financial_year, trading_record.price * trading_record.volume as f32);
+
+        let amount_settled = trading_record.price * trading_record.volume as f32;
+        self.consolidated_transactions.push(ConsolidatedTransaction {
+            date: trading_record.date,
+            trade_type: DIV,
+            code: String::from(&trading_record.code),
+            quantity: trading_record.volume,
+            price: trading_record.price,
+            fee: 0.0,
+            amount_settled,
+            fulfillments: None,
+            net_profit: amount_settled
+        });
     }
 
     fn record_fy_profit(&mut self, fy: u32, profit: f32) {
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ use prettytable::{Table, Cell};
 use prettytable::format;
 use prettytable::format::Alignment;
 use crate::inventory::Inventory;
-use prettytable::{Attr, color};
-use chrono::{NaiveDateTime, Datelike};
+use prettytable::{color};
+use chrono::{NaiveDateTime};
 use Alignment::RIGHT;
-use core::mem;
+use term::Attr::ForegroundColor;
 
 #[derive(Debug, Clone, Deserialize)]
 pub enum TradeType {
@@ -42,72 +42,61 @@ pub struct TradingRecord {
     fee: f32
 }
 
-fn calculate_buy_total(price: f32, volume: u32, fee: f32) -> f32 {
-    price * (volume as f32) + fee
-}
-fn calculate_sell_total(price: f32, volume: u32, fee: f32) -> f32 { price * (volume as f32) - fee }
-
 fn read_transactions() -> Result<(), Box<dyn Error>> {
     let mut inventory = Inventory::new();
 
     let mut table = Table::new();
-    table.set_titles(row!["Date", "Trade", "Code", "Volume", "Price", "Fee", "Total"]);
+    table.set_titles(row!["Date", "Trade", "Code", "Volume", "Price", "Fee", "Total", "Net Profit", "Fulfillment"]);
 
     let mut rdr = csv::Reader::from_reader(io::BufReader::new(File::open("trades.csv")?));
     for result in rdr.deserialize() {
         let record: TradingRecord = result?;
-        let record2 = record.clone();
-        inventory.record_transaction(&record2);
+        inventory.record_transaction(&record);
+    }
 
-        //let datetime = NaiveDateTime::parse_from_str(&record.date, "%Y-%m-%d %H:%M:%S").unwrap();
-        let mut row = row![record.date.format("%Y-%m-%d"), record.buy_or_sell, record.code];
-        row.add_cell(Cell::new_align(&record.volume.to_string(), RIGHT));
-        row.add_cell(Cell::new_align(&format!("{:.3}", &record.price), RIGHT));
-        row.add_cell(Cell::new_align(&format!("{:.2}", record.fee), RIGHT));
+    for t in inventory.consolidated_transactions().iter() {
+        let mut row = row![t.date.format("%Y-%m-%d"), t.trade_type, t.code];
+        row.add_cell(Cell::new_align(&t.quantity.to_string(), RIGHT));
+        row.add_cell(Cell::new_align(&format!("{:.3}", t.price), RIGHT));
 
-        let financial_year = if record.date.date().month() < 7 {record.date.date().year()} else {record.date.date().year() + 1} as u32;
-
-        match record.buy_or_sell {
+        match t.trade_type {
             TradeType::BUY => {
-                let total_string = format!("-{:.2}", calculate_buy_total(record.price, record.volume, record.fee));
-                inventory.buy(record.date, &record.code, record.volume, record.price, record.fee);
-                row.add_cell(Cell::new_align(&total_string, RIGHT));
+                row.add_cell(Cell::new_align(&format!("{:.2}", t.fee), RIGHT));
+                row.add_cell(Cell::new_align(&format!("-{:.2}", t.amount_settled), RIGHT));
+                row.add_cell(Cell::new(""));
                 table.add_row(row);
-            },
+            }
             TradeType::SELL => {
-                let total_string = format!("-{:.2}", calculate_sell_total(record.price, record.volume, record.fee));
-                let mut row = row![record.date.format("%Y-%m-%d"), record.buy_or_sell, record.code];
-                row.add_cell(Cell::new_align(&record.volume.to_string(), RIGHT));
-                row.add_cell(Cell::new_align(&format!("{:.2}", &record.price), RIGHT));
-                row.add_cell(Cell::new_align(&format!("{:.2}", record.fee), RIGHT));
-                row.add_cell(Cell::new_align(&total_string, RIGHT));
-                row.add_cell(Cell::new_align("Fulfill", RIGHT).with_style(Attr::ForegroundColor(color::CYAN)));
+                row.add_cell(Cell::new_align(&format!("{:.2}", t.fee), RIGHT));
+                row.add_cell(Cell::new_align(&format!("{:.2}", t.amount_settled), RIGHT));
+                let mut cell = Cell::new_align(&format!("{:.2}", t.net_profit), RIGHT);
+                if t.net_profit >= 0.0 {
+                    cell = cell.with_style(ForegroundColor(color::GREEN));
+                } else  {
+                    cell = cell.with_style(ForegroundColor(color::RED));
+                }
+                row.add_cell(cell);
                 table.add_row(row);
 
-                let fulfillment = inventory.sell(financial_year, &record.code, record.volume, record.price, record.fee);
-                let mut fulfillment_table = Table::new();
-                fulfillment_table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
-
-                for (quantity, bought_price, _bought_fee) in fulfillment.items {
-                    let mut row = row!["", "", "", "", "", "", ""];
-                    row.add_cell(Cell::new_align(&format!("{} x {:.2}", quantity, bought_price), RIGHT));
+                for f in t.fulfillments.as_ref().unwrap() {
+                    let mut row = row!["", "", "", "", "", "", "", ""];
+                    row.add_cell(Cell::new_align(&format!("{} x ({:.3} - {:.3}) = {:.2}", f.quantity, t.price, f.purchase_price, f.quantity as f32 * (t.price - f.purchase_price)), RIGHT));
+                    table.add_row(row);
+                    let mut row = row!["", "", "", "", "", "", "", ""];
+                    row.add_cell(Cell::new_align(&format!("Acquired duration: {} days", f.acquired_duration.num_days()), RIGHT));
+                    table.add_row(row);
+                    let mut row = row!["", "", "", "", "", "", "", ""];
+                    row.add_cell(Cell::new_align(&format!("Purchase fee: -{:.2}", f.purchase_fee), RIGHT));
+                    table.add_row(row);
+                    let mut row = row!["", "", "", "", "", "", "", ""];
+                    row.add_cell(Cell::new_align(&format!("Selling fee: -{:.2}", f.selling_fee), RIGHT));
                     table.add_row(row);
                 }
-                let mut row = row!["", "", "", "", "", "", ""];
-                let style = if fulfillment.net_profit < 0.0 { Attr::ForegroundColor(color::RED) } else { Attr::ForegroundColor(color::GREEN) };
-                let profit_or_loss = if fulfillment.net_profit < 0.0 { "Loss" } else { "Profit" };
-                row.add_cell(Cell::new_align(&format!("{}: {:.2}", profit_or_loss, fulfillment.net_profit), RIGHT).with_style(style));
-                table.add_row(row);
-            },
-            TradeType::DIV => { // dividend
-                inventory.record_dividend(financial_year, &record.code, record.volume, record.price);
-                let mut row = row![record.date.format("%Y-%m-%d"), TradeType::DIV, record.code];
-                row.add_cell(Cell::new_align(&record.volume.to_string(), RIGHT));
-                row.add_cell(Cell::new_align(&format!("{:.3}", &record.price), RIGHT));
+            }
+            TradeType::DIV => {
                 row.add_cell(Cell::new(""));
-                row.add_cell(Cell::new(""));
-                let total_string = format!("{:.2}", calculate_buy_total(record.price, record.volume, record.fee));
-                row.add_cell(Cell::new_align(&total_string, RIGHT).with_style(Attr::ForegroundColor(color::GREEN)));
+                row.add_cell(Cell::new_align(&format!("{:.2}", t.amount_settled), RIGHT));
+                row.add_cell(Cell::new_align(&format!("{:.2}", t.net_profit), RIGHT).with_style(ForegroundColor(color::GREEN)));
                 table.add_row(row);
             }
         }
@@ -128,5 +117,4 @@ fn main() {
         println!("error running example: {}", err);
         process::exit(1);
     }
-    println!("{}", mem::size_of::<NaiveDateTime>());
 }
